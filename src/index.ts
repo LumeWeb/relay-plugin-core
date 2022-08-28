e7aedc9950e726e4a64c977cfa26f3a246ff83ab
Derrick Hammer <derrick@derrickhammer.com>
2022-08-28 13:09:47 -0400

    *Use shorthand function

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-import type {Plugin, PluginAPI} from "@lumeweb/relay";
-import {RPCResponse} from "@lumeweb/relay";
+import type { Plugin, PluginAPI } from "@lumeweb/relay";
+import { RPCResponse } from "@lumeweb/relay";
 
 const plugin: Plugin = {
-    name: "core",
-    async plugin(api: PluginAPI): Promise<void> {
-        api.registerMethod("ping", {
-            cacheable: false,
-            handler: async (): Promise<RPCResponse | null> => {
-                return {
-                    data: "pong"
-                }
-            }
-        })
+  name: "core",
+  async plugin(api: PluginAPI): Promise<void> {
+    api.registerMethod("ping", {
+      cacheable: false,
+      async handler(): Promise<RPCResponse | null> {
+        return {
+          data: "pong",
+        };
+      },
+    });
 
-        api.registerMethod("get_methods", {
-            cacheable: false,
-            handler: async (): Promise<RPCResponse | null> => {
-                return {
-                    data: api.getMethods()
-                }
-            }
-        })
-    }
-
-}
+    api.registerMethod("get_methods", {
+      cacheable: false,
+      async handler(): Promise<RPCResponse | null> {
+        return {
+          data: api.getMethods(),
+        };
+      },
+    });
+  },
+};
 
 export default plugin;
